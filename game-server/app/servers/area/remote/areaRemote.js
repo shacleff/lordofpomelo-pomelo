326dadcd0739295b933fe9581984c7739a93ee80
jianan3 <jianan3@vipkid.com.cn>
2019-08-15 13:07:09 +0800

    area

--- game-server/app/servers/area/remote/areaRemote.js
+++ game-server/app/servers/area/remote/areaRemote.js
@@ -5,20 +5,20 @@ var logger = require('pomelo-logger').getLogger(__filename);
 var exp = module.exports;
 
 exp.create = function (params, cb) {
-  var start = Date.now();
-  var result = instancePool.create(params);
-  var end = Date.now();
-  logger.info('create instance use time : %j', end - start);
+    var start = Date.now();
+    var result = instancePool.create(params);
+    var end = Date.now();
+    logger.info('create instance use time : %j', end - start);
 
-  utils.invokeCallback(cb, null, result);
+    utils.invokeCallback(cb, null, result);
 };
 
 exp.close = function (params, cb) {
-  var id = params.id;
-  var result = instancePool.close(id);
+    var id = params.id;
+    var result = instancePool.close(id);
 
-  // 作用类似于cb(null,  result)
-  utils.invokeCallback(cb, null, result);
+    // 作用类似于cb(null,  result)
+    utils.invokeCallback(cb, null, result);
 };
 
 
